612b748a4f70f223b98e41390565842fba27df15
Jacob Wilson <jacobleeeugenewilson@gmail.com>
2021-11-12 13:40:43 -0900

    add red test for create a planet object...

--- __tests__/planetConverter.test.js
+++ __tests__/planetConverter.test.js
@@ -1,4 +1,11 @@
 import PlanetConverter from '../src/js/planetConverter';
+import Person from '../src/js/person';
+
 describe('PlanetConverter', () => {
-  test('should create a planet object that contains a personObject and a map of planet year conversions ')
+  
+  test('should create a planet object that contains a personObject and a map of planet year conversions', () =>{
+    let healthyMale = new Person(26, "male", 62, 130, false, 1);
+    const planetConverter = new PlanetConverter(healthyMale);
+    expect(planetConverter.person.age).toEqual(26);
+  });
 });

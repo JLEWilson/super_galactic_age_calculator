47963c4ff8abd6ab47c29e5851bedb09e4c4b3c2
Jacob Wilson <jacobleeeugenewilson@gmail.com>
2021-11-12 11:25:46 -0900

    add red test for calculateLifeExpectancy based on weight, update variables

--- __tests__/age_calculator.tests.js
+++ __tests__/age_calculator.tests.js
@@ -3,37 +3,46 @@ import {Person} from './../src/age_calculator';
 
 describe('Person', () => {
 
+  let healthyMale;
+  let healthyFemale;
+
+  beforeEach(() => {
+    healthyMale = new Person(26, "male", 62, 160, false, 1);
+    healthyFemale = new Person(26, "female", 60, 115, false, 1);
+  });
+
   test('should correctly create a person object with an age property', () => {
     const person = new Person(26);
     expect(person.age).toEqual(26);
   });
 
   test('should correctly create a person object with age, sex, height, weight, isSmoker, and drinksPerWeek properties', () => {
-    person = new Person(26, "male", 71, 175, false, 1);
-    expect(person.age).toEqual(26);
-    expect(person.sex).toEqual("male");
-    expect(person.height).toEqual(71);
-    expect(person.weight).toEqual(175);
-    expect(person.isSmoker).toEqual(false);
-    expect(person.drinksPerWeek).toEqual(1);
+    expect(healthyMale.age).toEqual(26);
+    expect(healthyMale.sex).toEqual("male");
+    expect(healthyMale.height).toEqual(62);
+    expect(healthyMale.weight).toEqual(160);
+    expect(healthyMale.isSmoker).toEqual(false);
+    expect(healthyMale.drinksPerWeek).toEqual(1);
   });
     
 
   test('calculateLifeExpectancy method should calculate expected life duration based on sex property and set outcome as lifeExpectancy property', () => {
-    const person = new Person(26, "male", 62, 175, false, 1);
-    const person2 = new Person(26, "female", 60, 120, true, 10);
-    person.calculateLifeExpectancy();
-    person2.calculateLifeExpectancy();
-    expect(person.lifeExpectancy).toEqual(76.1);
-    expect(person2.lifeExpectancy).toEqual(81.1);
+    healthyMale.calculateLifeExpectancy();
+    healthyFemale.calculateLifeExpectancy();
+    expect(healthyMale.lifeExpectancy).toEqual(76.1);
+    expect(healthyFemale.lifeExpectancy).toEqual(81.1);
   });
 
-  test('calculateLifeExpectancy method should also calculate expected life duration based on height property', () => {
-    const person = new Person(26, "male", 75, 175, false, 1); // height above range for negative longevity
-    const person2 = new Person(26, "female", 60, 120, true, 10);
-    person.calculateLifeExpectancy();
-    person2.calculateLifeExpectancy();
-    expect(person.lifeExpectancy).toBeLessThan(76.1);
-    expect(person2.lifeExpectancy).toEqual(81.1);
+  test('calculateLifeExpectancy method should also calculate expected life duration based on height being above 62 inches', () => {
+    const tallPerson = new Person(26, "male", 75, 175, false, 1); // height above range for negative longevity
+    tallPerson.calculateLifeExpectancy();
+    expect(tallPerson.lifeExpectancy).toBeLessThan(76.1);
+  });
+
+  test('calculateLifeExpectancy method should also calculate expected life duration based on weight, seeing if bmi is under healthy range', () => {
+    const underWeightPerson = new Person(26, "male", 62, 60, false, 1); 
+
+    underWeightPerson.calculateLifeExpectancy();
+    expect(underWeightPerson.lifeExpectancy).toBeLessThan(76.1);
   });
 });

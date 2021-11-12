b56de557876651107be40535966f026a69672545
Jacob Wilson <jacobleeeugenewilson@gmail.com>
2021-11-12 08:58:21 -0900

    add green test for calculateLifeExpexctancy

--- src/age_calculator.js
+++ src/age_calculator.js
@@ -6,10 +6,25 @@ export class Person{
     this.weight = weight;
     this.isSmoker = isSmoker;
     this.drinksPerWeek = drinksPerWeek;
+    this.lifeExpectancy = this.calculateLifeExpectancy();
   }
 
   calculateLifeExpectancy() {
+    let lifeExpectancy = 0;
 
+    //Calculate based on sex. Data from 2017, resource used https://www.advisory.com/en/daily-briefing/2020/07/22/longevity#:~:text=As%20of%202017%2C%20life%20expectancy,with%2083.9%20years%20for%20men.
+    switch(this.sex){
+      case "male":
+        lifeExpectancy += 76.1;
+        break;
+      case "female":
+        lifeExpectancy += 81.1;
+        break;
+      default:
+        return null;  //Unfortunately not enough data me to account for interex individuals at this time
+    }
+
+    return lifeExpectancy;
   }
 }
 

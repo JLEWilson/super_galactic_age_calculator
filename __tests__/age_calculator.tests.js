202c96b8f2cde937cc74496a02611c10700a70f7
Jacob Wilson <jacobleeeugenewilson@gmail.com>
2021-11-12 08:27:13 -0900

    add red test for person with additional properties

--- __tests__/age_calculator.tests.js
+++ __tests__/age_calculator.tests.js
@@ -4,6 +4,17 @@ describe('Person', () => {
 
   test('should correctly create a person object with an age property', () => {
     const person = new Person(26);
-    expect(person.earthAge).toEqual(26);
+    expect(person.age).toEqual(26);
   });
+
+  test('should correctly create a person object with age, sex, height, weight, isSmoker, and drinksPerWeek properties', () => {
+    const person = new Person(26, "male", 71, 175, false, 1);
+    expect(person.age).toEqual(26);
+    expect(person.sex).stringMatching("male");
+    expect(person.height).toEqual(71);
+    expect(person.weight).toEqual(175);
+    expect(person.isSmoker).toEqual(false);
+    expect(person.drinksPerWeek).toEqual(1);
+  });
+    
 });

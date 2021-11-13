03b57bcb8551979f0f691f5520b1d80ef358ded6
Jacob Wilson <jacobleeeugenewilson@gmail.com>
2021-11-12 15:54:31 -0900

    WIP add form and base styling

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
+import $ from 'jquery';
+import Person from './js/person';
+import PlanetConverter from './js/planetConverter';
+
+let person;
+let planetConverter;
